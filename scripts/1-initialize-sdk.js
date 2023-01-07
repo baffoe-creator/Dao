16c8158c539a3b4a571773b143e1ddd42a63b5a2
baffoe-creator <baffoekuuku9@gmail.com>
2023-01-07 23:02:02 +0000

    first commit

---
+++ scripts/1-initialize-sdk.js
@@ -0,0 +1,33 @@
+import { ThirdwebSDK } from "@thirdweb-dev/sdk";
+
+// Importing and configuring our .env file that we use to securely store our environment variables
+import dotenv from "dotenv";
+dotenv.config();
+
+// Some quick checks to make sure our .env is working.
+if (!process.env.PRIVATE_KEY || process.env.PRIVATE_KEY === "") {
+  console.log("🛑 Private key not found.");
+}
+
+if (!process.env.QUICKNODE_API_URL || process.env.QUICKNODE_API_URL === "") {
+  console.log("🛑 QuickNode API URL not found.");
+}
+
+if (!process.env.WALLET_ADDRESS || process.env.WALLET_ADDRESS === "") {
+  console.log("🛑 Wallet Address not found.");
+}
+
+const sdk = ThirdwebSDK.fromPrivateKey("PRIVATE_KEY", "goerli");
+
+(async () => {
+  try {
+    const address = await sdk.getSigner().getAddress();
+    console.log("👋 SDK initialized by address:", address)
+  } catch (err) {
+    console.error("Failed to get apps from the sdk", err);
+    process.exit(1);
+  }
+})();
+
+// We are exporting the initialized thirdweb SDK so that we can use it in our other scripts
+export default sdk;
